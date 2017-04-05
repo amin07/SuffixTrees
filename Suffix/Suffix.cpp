aa7da575ef5ea925d84e3e9414c097712e06e0ae
admin_user <admin_user@ENGR5325-06.mesa.gmu.edu>
2017-04-05 14:39:27 -0400

    Suffix tree - n^2 time solution added

--- Suffix/Suffix.cpp
+++ Suffix/Suffix.cpp
@@ -1,21 +1,89 @@
 #include "Suffix.h"
 
+int numOfMatchedChar(string str, int start_pos, string str2)
+{
+	int ret=0;
+	for(int i=0;i<str2.length();i++){
+		if(str2[i]!=str[i+start_pos]) break;
+		ret++;
+	}
+	return ret;
+}
 
-TN buildTree(string str){
+void addSuffix(string str, int start_pos, int suffix_label, TN *currNode){
+
+	// checking matching child node
+
+	bool matchedFlag = false;
+	for(int i=0 ;i<currNode->childs.size(); i++){
+		TN *n = currNode->childs[i];
+
+		if(str.at(start_pos)==n->edge_label.at(0)){
+			int matchedLen = numOfMatchedChar(str, start_pos, n->edge_label);
+			if(matchedLen==n->edge_label.length()){
+				addSuffix(str, start_pos+matchedLen, suffix_label, n);
+			}
+			else
+			{
+				// breaking the edge
+				TN *newRoot = new TN(-1, str.substr(start_pos, matchedLen));
+				//TN *newRoot = new TN(-1, "dummy");
+				//n->updateLabel(n->edge_label.substr(start_pos+matchedLen));
+				n->updateLabel(n->edge_label.substr(matchedLen));
+				TN *modifiedChild = n;
+				TN *newChild = new TN(suffix_label, str.substr(start_pos+matchedLen));
+
+				newRoot->addChild(modifiedChild);
+				newRoot->addChild(newChild);
+
+				// deleting previous link and adding new link
+				swap(currNode->childs[i], currNode->childs.back());
+				currNode->childs.pop_back();
+				currNode->addChild(newRoot);
+			}
+			matchedFlag = true;
+			break;
+		}
+	}
+
+	if(matchedFlag==false){
+		currNode->addChild(new TN(suffix_label, str.substr(start_pos)));
+	}
+
+}
+
+
+TN * buildTree(string str){
 	TN *root = new TN();
 	root->addChild(new TN(0,str));	// adding full suffix
 
 	for(int i=1;i<str.size();i++){
-
+		addSuffix(str, i,i,root);
 	}
 
 	return root;
 }
 
+
+void printSuffixes(TN *nd, string runString){
+	if(nd->nodeMark>=0){
+		cout<<runString<<" "<<nd->nodeMark<<endl;
+	}
+	for(auto n : nd->childs){
+		//cout<<"This is an edge label: "<<n->edge_label<<endl;
+		printSuffixes(n, runString+n->edge_label);
+	}
+
+}
+
+
 int main(){
-	string str = "hello world here";
+	string str = "axybxyc";
 	str.append("#");
 
-	cout<<str<<endl;
+	TN *tree = buildTree(str);
+	printSuffixes(tree, "");
+
+
 	return 0;
 }
--- Suffix/Suffix.h
+++ Suffix/Suffix.h
@@ -9,6 +9,7 @@ struct TreeNode
     string edge_label;
     TreeNode(){nodeMark=-1;edge_label="";}
     TreeNode(int n, string label):nodeMark(n),edge_label(label){}
-    void addChild(TN *node){this->childs.push_back(node);}
+    void addChild(TreeNode *node){this->childs.push_back(node);}
+    void updateLabel(string n){edge_label=n;}
 };
 typedef TreeNode TN;
